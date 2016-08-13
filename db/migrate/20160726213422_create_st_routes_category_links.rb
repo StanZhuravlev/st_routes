0931d6643bd81d3da28cd255d89256ce154919de
StanZhuravlev <stan@post-api.ru>
2016-08-13 09:43:12 +0300

    Исправлена ошибка с eager_load

--- db/migrate/20160726213422_create_st_routes_category_links.rb
+++ db/migrate/20160726213422_create_st_routes_category_links.rb
@@ -7,8 +7,8 @@ class CreateStRoutesCategoryLinks < ActiveRecord::Migration[5.0]
       t.timestamps
     end
 
-    add_foreign_key :st_routes_category_links, :categories, column: :category_id
-    add_foreign_key :st_routes_category_links, :parent_categories, column: :parent_category_id
+    add_foreign_key :st_routes_category_links, :st_routes_categories, column: :category_id
+    add_foreign_key :st_routes_category_links, :st_routes_categories, column: :parent_category_id
     add_index :st_routes_category_links, [:category_id, :parent_category_id], name: "category_child_parent_idx", unique: true
   end
 end
