1cf568755c3fb2c39d5fea7420ed28f869ea4ba6
StanZhuravlev <stan@post-api.ru>
2016-08-13 10:19:12 +0300

    Исправлена ошибка с eager_load

--- app/models/st_routes/category_link.rb
+++ app/models/st_routes/category_link.rb
@@ -3,5 +3,13 @@ module StRoutes
     belongs_to :parent_category, class_name: 'Category'
     belongs_to :category
 
+    after_save :rebuild_categories_urls
+    after_destroy :rebuild_categories_urls
+
+    def rebuild_categories_urls
+      builder = StRoutes::URL::Builder.new
+      builder.rebuild_categories_urls
+    end
+
   end
 end
--- lib/st_routes/version.rb
+++ lib/st_routes/version.rb
@@ -1,3 +1,3 @@
 module StRoutes
-  VERSION = '0.1.9'
+  VERSION = '0.1.10'
 end
